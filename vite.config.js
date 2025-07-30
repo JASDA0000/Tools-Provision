785e2a6c97cb073597578337e6943c47ae0f58ce
JASDA0000 <103983336+JASDA0000@users.noreply.github.com>
2025-07-30 16:00:50 +0700

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(),tailwindcss()],
+  resolve :{
+    alias : {
+      '@View' : '/src/views'
+    }
+  },
   server:{
     proxy:{
       '/sheet':'https://localhost:5000',
