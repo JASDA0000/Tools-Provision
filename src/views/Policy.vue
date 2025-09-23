e7c59ce4dfd6bf8ce293420be1c0cebd228992e6
JASDA0000 <103983336+JASDA0000@users.noreply.github.com>
2025-09-23 16:11:09 +0700

    Update Policy.vue

--- src/views/Policy.vue
+++ src/views/Policy.vue
@@ -14,6 +14,7 @@ import Card from '../components/Card.vue';
           <option value="dedicate">IP Dedicate</option>
         </select>
         </div>
+        
         </Card>
     </div>
 </template>
